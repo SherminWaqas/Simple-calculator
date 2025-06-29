a49eba695aa596deda3889f77a0f77238d1b7156
SherminWaqas <sherminwaqas14@gmail.com>
2025-06-29 12:56:54 +0300

    Add files via upload

---
+++ simple_calculator.py
@@ -0,0 +1,36 @@
+# Simple Calcutor in Python using input and if-else statements
+
+# Taking input from the user for the first number
+Number1 = float(input("Enter the first number: "))
+
+# Taking input from the user for th second number
+Number2 + float(input("Enter the second number:"))
+
+# Asking the user to choose an operation
+print("Choose an operation: + - * /")
+operation = input ("Enter operation: ")
+
+# Using if-elif-else to perform the selected operation
+if operation == "+" :
+    result = Number1 + Number2
+    print("The result is:", result)
+
+elif operation == "-":
+    result = Number1 - Number2
+    print("The result is:", result)
+
+elif operation == "*":
+    result = Number1 * Number2
+    print("The result is:", result)
+
+elif operation == "/":
+    # Check if the second number is not zero to avoid division error
+    if Number2 != 0:
+        result = Number1 / Number2
+        print("The result is:", result)
+    else:
+        print("Error: Cannot divide by zero. ")
+
+    else:
+# If the user enter an invalid operation
+print("Invalid operation selected.")
